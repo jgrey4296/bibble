81d57885ed7ee841a0bea7583c36d9c99b31e036
John Grey <johngrey4296@gmail.com>
2024-11-18 20:34:38 +0000

    [add]: FieldMatcher_m

---
+++ bib_middleware/util/field_matcher.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python3
+"""
+
+
+
+"""
+
+from __future__ import annotations
+
+import datetime
+import enum
+import functools as ftz
+import itertools as itz
+import logging as logmod
+import pathlib as pl
+import re
+import time
+import types
+import weakref
+from typing import (TYPE_CHECKING, Any, Callable, ClassVar, Final, Generator,
+                    Generic, Iterable, Iterator, Mapping, Match, Self,
+                    MutableMapping, Protocol, Sequence, Tuple, TypeAlias,
+                    TypeGuard, TypeVar, cast, final, overload,
+                    runtime_checkable)
+from uuid import UUID, uuid1
+
+import bibtexparser
+import bibtexparser.model as model
+from bibtexparser import exceptions as bexp
+from bibtexparser import middlewares as ms
+from bibtexparser.model import MiddlewareErrorBlock
+from bibtexparser.middlewares.middleware import BlockMiddleware, LibraryMiddleware
+from bibtexparser.middlewares.names import parse_single_name_into_parts, NameParts
+
+##-- logging
+logging = logmod.getLogger(__name__)
+##-- end logging
+
+class FieldMatcher_m:
+    """ Mixin to process fields if their key matchs a regex
+    defaults are in the attrs _field_blacklist and _field_whitelist
+    Call set_field_matchers to extend.
+    Call match_on_fields to start.
+    Implement field_handler to use.
+
+    match_on_fields calls entry.set_field on the field_handlers result
+    """
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.set_field_matchers([],[])
+
+    def set_field_matchers(self, white:list[str], black:list[str], keep_default=True) -> Self:
+        """ sets the blacklist and whitelist regex's
+        returns self to help in building parse stacks
+        """
+        match keep_default, getattr(self, "_field_blacklist", []):
+            case _, [] if not bool(black):
+                self._field_black_re = re.compile("^$")
+            case _, []:
+                self._field_black_re = re.compile("|".join(list(black)))
+            case False, _:
+                self._field_black_re = re.compile("|".join(list(black)))
+            case True, [*xs] as defs:
+                self._field_black_re = re.compile("|".join(list(black) + defs))
+
+        match keep_default, getattr(self, "_field_whitelist", []):
+            case _, [] if not bool(white):
+                self._field_white_re = re.compile(".")
+            case _, []:
+                self._field_white_re = re.compile("|".join(list(white)))
+            case False, _:
+                self._field_white_re = re.compile("|".join(list(white)))
+            case True, [*xs] as defs:
+                self._field_white_re = re.compile("|".join(list(white) + defs))
+
+        return self
+
+
+    def match_on_fields(self, entry: Entry, library: Library) -> tuple[Entry, list[str]]:
+        errors = []
+        whitelist, blacklist = self._field_white_re, self._field_black_re
+        for field in entry.fields:
+            match field:
+                case model.Field(key=key) if blacklist.match(key):
+                    continue
+                case model.Field(key=key) if whitelist.match(key):
+                    res, errs = self.field_handler(field)
+                    errors += errs
+                    entry.set_field(res)
+
+        else:
+            return entry, errors
+
+
+    def field_handler(self, field:model.Field) -> tuple[model.Field, list[str]]:
+        raise NotImplementedError("Implement the field handler")
