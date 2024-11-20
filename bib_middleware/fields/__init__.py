d62b3796da692d1d6d9cbc68da4b3bf75056bf45
John Grey <johngrey4296@gmail.com>
2024-11-20 19:06:52 +0000

    [add]: field sorter

--- bib_middleware/fields/__init__.py
+++ bib_middleware/fields/__init__.py
@@ -6,3 +6,4 @@ from .subtitle_reader import SubTitleReader
 from .title_reader import TitleReader
 from .url_reader import CleanUrls
 from .field_substitutor import FieldSubstitutor
+from .field_sorter import FieldSorter
